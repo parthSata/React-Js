04a6f88a8c7b978865770e26df710dead840a728
Hemal <Hemal@DESKTOP-UN8MBH0>
2024-01-31 19:52:10 +0530

    currency convertor

--- 07_CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ 07_CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 
-function useCurrencyInfo(){
-    
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    const api = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    useEffect(() => {
+
+        fetch(api)
+            .then((response) => response.json())
+            .then((responseData) => setData(responseData[currency]))
+            
+            console.log(data)
+
+    }, [currency])
+    console.log(data)
+    return data
 }
+export default useCurrencyInfo;
