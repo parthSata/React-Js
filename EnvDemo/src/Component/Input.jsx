57431626b60d3cd7ebd098992f9c69ae59b970d3
joshihemal <hemaljoshi738@gmail.com>
2024-02-23 20:41:36 +0530

    Refactor Input component and update .env file

--- EnvDemo/src/Component/Input.jsx
+++ EnvDemo/src/Component/Input.jsx
@@ -1,34 +1,26 @@
-import React, { forwardRef, useId } from 'react'
+import React, { forwardRef, useId } from "react";
 
-const Input = forwardRef(({
-    label,
-    type = 'text',
-    classname = '',
-    ...props
-}, ref) => {
-
-    const id = useId()
+const Input = forwardRef(
+  ({ label, type = "text", classname = "", ...props }, ref) => {
+    const id = useId();
     return (
-        <>
-            <div className="w-full">
-                {label && <label
-                    className='inline-block mb-1 pl-1'
-                    htmlFor={id}
-                >
-                    {label}
-                </label>
-                }
+      <div className="w-full">
+        {label && (
+          <label className="inline-block mb-1 pl-1" htmlFor={id}>
+            {label}
+          </label>
+        )}
 
-                <input
-                    type={type}
-                    className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full  ${classname}`}
-                    ref={ref}
-                    {...props}
-                    id={id}
-                />
-            </div>
-        </>
-    )
-})
+        <input
+          type={type}
+          className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full  ${classname}`}
+          ref={ref}
+          {...props}
+          id={id}
+        />
+      </div>
+    );
+  }
+);
 
-export default Input
+export default Input;
